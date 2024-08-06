2bfef89e5bec8972f2b9afea8f7b1eed9a5073e0
Da7eddine <yassineboudidah@gmail.com>
2024-08-06 08:37:47 +0100

    hero section preperation

--- admin/src/app/components/navbar/page.tsx
+++ admin/src/app/components/navbar/page.tsx
@@ -1,63 +1,41 @@
-'use server'
-import Image from 'next/image'
-import Logo from '../../../public/image/falleh.png'
-import Link from 'next/link'
-import ProfileImage from '../../../public/image/blank-profile-picture-973460_960_720.webp'
-import {cookies} from 'next/headers'
+import Link from "next/link"
+import Image from "next/image"
 
-
-const Navbar = () => {
-const GetUser =cookies().get("Authorization")
-const user = GetUser !== undefined ? GetUser.value : null;
-
+export default function page  (){
   return (
-    <header className="fixed w-full">
-        <nav className="bg-white border-gray-200 py-2.5">
-            <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
-                <Link href="/" className="flex items-center">
-                    <Image src={Logo} className="w-[120px] auto ml-1 sm:w-[134px]" alt="Landwind Logo" />
-                </Link>
-                <div className="flex items-center lg:order-2">
-                
-                 {!user && 
-                 <>
-                 <Link href="/login" className="text-[#000]  font-medium rounded-lg text-sm mr-2 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 hover:text-[#058f1a] ">Login</Link>
-                <Link href="/signup" className="text-white bg-[#058f1a] focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm  lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0">Signup</Link>
-                </>
-                }
-                
-        <button   type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden " >
-                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                    </button>
-                </div>
-                <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                    <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li>
-                            <Link href="/" className="block py-2 pl-3 pr-4 text-[16px] text-white bg-[#058f1a] rounded lg:bg-transparent lg:text-[#000] lg:p-0 hover:text-[#058f1a]">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/news" className="block py-2 pl-3 pr-4 text-[16px] text-white bg-[#058f1a] rounded lg:bg-transparent lg:text-[#000] lg:p-0 hover:text-[#058f1a]">News</Link>
-                        </li>
-                        <li>
-                            <Link href="/marketplace" className="block py-2 pl-3 pr-4 text-[16px] text-white bg-[#058f1a] rounded lg:bg-transparent lg:text-[#000] lg:p-0 hover:text-[#058f1a] ">Marketplace</Link>
-                        </li>
-                        <li>
-                            <Link href="/community" className="block py-2 pl-3 pr-4 text-[16px] text-white bg-[#058f1a] rounded lg:bg-transparent lg:text-[#000] lg:p-0 hover:text-[#058f1a]">Community</Link>
-                        </li>
-                        <li>
-                            <Link href="about" className="block py-2 pl-3 pr-4 text-[16px] text-white bg-[#058f1a] rounded lg:bg-transparent lg:text-[#000] lg:p-0 hover:text-[#058f1a]">About</Link>
-                        </li>
-                        <li>
-                            <Link href="/contact" className="block py-2 pl-3 pr-4 text-[16px]  text-white bg-[#058f1a] rounded lg:bg-transparent lg:text-[#000] lg:p-0 hover:text-[#058f1a]">Contact</Link>
-                        </li>
-                        
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+<nav className="bg-white border-gray-200 dark:bg-gray-900">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <Link href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Image src="" className="h-8" alt="Flowbite Logo" />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Falleh Bledi</span>
+    </Link>
+    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <Link href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+        </li>
+        <li>
+          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
+        </li>
+        <li>
+          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</Link>
+        </li>
+        <li>
+          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</Link>
+        </li>
+        <li>
+          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
   )
 }
-
-export default Navbar
