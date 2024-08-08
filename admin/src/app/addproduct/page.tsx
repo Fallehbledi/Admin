4bfd61fc4599be00d8ef54e6daed990df8820184
fathi777b <barhoumifathi2021@gmail.com>
2024-08-08 12:25:05 +0200

    add product with firebase done

--- admin/src/app/addproduct/page.tsx
+++ admin/src/app/addproduct/page.tsx
@@ -1,43 +1,16 @@
 "use client"
-import axios from 'axios';
-
-import React, { useState } from 'react';
+import AddProdAction from './addProductAction'
 
 
 const AddProduct = () => {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [image , setImage] = useState('')
-  const [price, setPrice] = useState('');
-  const [category, setCategory] = useState('');
 
-
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://127.0.0.1:5000/api/tools/add', {
-        name,
-        description,
-         image ,
-        price,
-        category,
-        quantity,
-      });
-      console.log('Product added:', response.data);
-      // Redirect to the marketplace
-     
-    } catch (error) {
-      console.error('Error adding product:', error);
-    }
-  };
+ 
 
   return (
     <div className="max-w-lg mx-auto bg-white p-6 md:p-8 border border-gray-200 rounded-lg shadow-md mt-8">
        
       <h2 className="text-2xl font-bold mb-4 md:mb-6 text-center md:text-left">Add New Product</h2>
-      <form onSubmit={handleSubmit}>
+      <form action={AddProdAction} >
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="productName">
             Product Name
@@ -46,9 +19,8 @@ const AddProduct = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="productName"
             type="text"
+            name='name'
             placeholder="Product Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
           />
         </div>
 
@@ -60,22 +32,9 @@ const AddProduct = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="category"
             type="text"
+            name = "category"
             placeholder="Category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="image">
-           image
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="image"
-            type="text"
-            placeholder="image"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
+            
           />
         </div>
 
@@ -87,9 +46,9 @@ const AddProduct = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="price"
             type="number"
+            name = "price"
             placeholder="Price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+           
           />
         </div>
 
@@ -100,10 +59,10 @@ const AddProduct = () => {
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="quantity"
+            name="quantity"
             type="number"
             placeholder="Quantity"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
+            
           />
         </div>
 
@@ -115,8 +74,7 @@ const AddProduct = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="description"
             placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            name = "description"
           />
         </div>
 
@@ -128,7 +86,7 @@ const AddProduct = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="image"
             type="file"
-            
+            name='image'
           />
         </div>
 
